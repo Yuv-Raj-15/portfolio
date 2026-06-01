53025096d56b5fe5e0476139ca5bc4be583f9ac9
Yuvraj Rauniyar <yuvrajrauniyar15@gmail.com>
2026-06-01 23:21:14 +0545

    js improved

--- mail.js
+++ mail.js
@@ -2,11 +2,16 @@ let btn = document.getElementById("sbmt");
 
 btn.addEventListener("click", function (e) {
     e.preventDefault();
+    let x=document.getElementById("name").value;
+    let y=document.getElementById("email").value;
+    let z=document.getElementById("msg").value;
     var templateParams = {
-        name: document.getElementById("name").value,
-        Email: document.getElementById("email").value,
-        notes: document.getElementById("msg").value,
+        name: x,
+        Email: y,
+        notes: z,
     };
+    if(x==""||y==""||z=="") btn.innerText="Incomplete Info";
+    else{
     emailjs.send('service_ru40mfk', 'template_4lznjbh', templateParams)
     .then(function(){
         btn.style.backgroundColor="#7bf1a8";
@@ -16,6 +21,6 @@ btn.addEventListener("click", function (e) {
     .catch(function(){
         btn.style.backgroundColor="red";
         btn.innerText="Unable To Send";
-    })
+    })}
     
 });
